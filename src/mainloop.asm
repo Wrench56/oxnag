1ac711901a7647ffaa7f7786395aaec454114ae9
Mark Devenyi <markdevenyidev@gmail.com>
2026-08-17 21:00:38 -0400

    feat: draw 3D Wavefront model onto canvas in `mainloop.asm`

--- src/mainloop.asm
+++ src/mainloop.asm
@@ -3,9 +3,18 @@ extern glLoadIdentity
 
 extern glGetError
 
+extern glUseProgram
+extern glBindVertexArray
+extern glDrawElements
+extern glBindVertexArray
+
+extern upload_obj_to_gpu
+extern create_shader_program
+
 %include "src/flags.inc"
 %include "includes/common/macros.inc"
-%include "includes/common/opengl.inc"   
+%include "includes/common/opengl.inc"
+%include "includes/common/object.inc"
 
 %ifidn TARGET_OS, OS_WINDOWS
     extern whandle_win_events
@@ -29,14 +38,24 @@ extern glGetError
 section .data
     glFatalTitle        db "Error: mainloop.asm", 0
     glErrMessage        db "[ mainloop ]  OpenGL Error", 0
-
-
+    test_obj_file       db "assets/h2r.obj", 0
+    shader_program      dq 0
+    object              dq 0
 
 section .text
 
 extern mainloop
 mainloop:
     prologue        32
+
+    call            create_shader_program
+    mov             [shader_program], rax
+
+    mov             arg(1), test_obj_file
+    call            upload_obj_to_gpu
+    mov             [object], rax
+
+
 
 .mloop:
     ; Call OS specific window handling
@@ -57,6 +76,20 @@ mainloop:
 
     ; Reset the current Modelview matrix
     call            glLoadIdentity
+
+    mov             arg(1), [shader_program]
+    call            glUseProgram
+
+    mov             rax, [rel object]
+    mov             arg(1), [rax + Object.vao]
+    call            glBindVertexArray
+
+    mov             rax, [rel object]
+    mov             arg(1), GL_TRIANGLES
+    mov             arg(2), [rax + Object.index_count]
+    mov             arg(3), GL_UNSIGNED_INT
+    xor             arg(4), arg(4)
+    call            glDrawElements
 
     ; Swap buffers (OS specific)
     swap_buffers
